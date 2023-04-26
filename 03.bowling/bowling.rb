119560b896090d1e92026f386ee2f50f4923ff3e
Hiroaki Kadomatsu <note103@gmail.com>
2023-04-26 19:54:35 +0900

    ポイントの計算部分をリファクタリング

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 # frozen_string_literal: true
 
 score = ARGV[0]
@@ -23,17 +24,17 @@ frames.each_with_index do |frame, index|
   break if index == 10
 
   next_frame = frames[index + 1]
-  if frame[0] == 10 # strike
-    if next_frame[0] == 10
-      point += 10 + next_frame[0] + frames[index + 2][0]
-    else
-      point += 10 + next_frame.sum
-    end
-  elsif frame.sum == 10 # spare
-    point += 10 + next_frame[0]
-  else
-    point += frame.sum
-  end
+  point += if frame[0] == 10 # strike
+             if next_frame[0] == 10
+               10 + next_frame[0] + frames[index + 2][0]
+             else
+               10 + next_frame.sum
+             end
+           elsif frame.sum == 10 # spare
+             10 + next_frame[0]
+           else
+             frame.sum
+           end
 end
 
 puts point
@@ -46,3 +47,10 @@ __END__
 134: ruby bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0
 144: ruby bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8
 300: ruby bowling.rb X,X,X,X,X,X,X,X,X,X,X,X
+
+# scores = %w[6 3 9 0 0 3 8 2 7 3 X 9 1 8 0 X 6 4 5] # 139
+# scores = %w[6 3 9 0 0 3 8 2 7 3 X 9 1 8 0 X X X X] # 164
+# scores = %w[0 10 1 5 0 0 0 0 X X X 5 1 8 1 0 4] # 107
+# scores = %w[6 3 9 0 0 3 8 2 7 3 X 9 1 8 0 X X 0 0] # 134
+# scores = %w[6 3 9 0 0 3 8 2 7 3 X 9 1 8 0 X X 1 8] # 144
+# scores = %w[X X X X X X X X X X X X] # 300
