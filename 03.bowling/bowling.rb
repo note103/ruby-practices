06603171e985a1886306382070fccb0dd4d8e7e7
Hiroaki Kadomatsu <note103@gmail.com>
2023-04-29 15:32:20 +0900

    確認用データを削除

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -38,19 +38,3 @@ frames.each_with_index do |frame, index|
 end
 
 puts point
-
-__END__
-確認用データ
-139: ruby bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5
-164: ruby bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X
-107: ruby bowling.rb 0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4
-134: ruby bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0
-144: ruby bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8
-300: ruby bowling.rb X,X,X,X,X,X,X,X,X,X,X,X
-
-# scores = %w[6 3 9 0 0 3 8 2 7 3 X 9 1 8 0 X 6 4 5] # 139
-# scores = %w[6 3 9 0 0 3 8 2 7 3 X 9 1 8 0 X X X X] # 164
-# scores = %w[0 10 1 5 0 0 0 0 X X X 5 1 8 1 0 4] # 107
-# scores = %w[6 3 9 0 0 3 8 2 7 3 X 9 1 8 0 X X 0 0] # 134
-# scores = %w[6 3 9 0 0 3 8 2 7 3 X 9 1 8 0 X X 1 8] # 144
-# scores = %w[X X X X X X X X X X X X] # 300
