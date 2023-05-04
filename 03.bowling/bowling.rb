a79dd1a0ebeb4e2fdf4b147120d6902fbb51be85
Hiroaki Kadomatsu <note103@gmail.com>
2023-05-04 14:25:37 +0900

    不要な空行をカット

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -17,7 +17,6 @@ end
 frames = shots.each_slice(2).to_a
 
 point = frames.take(10).each_with_index.sum do |frame, index|
-
   next_frame = frames[index + 1]
   if frame[0] == 10 # strike
     if next_frame[0] == 10
