363b92ee8a7dfc53feca8578a2043a2428642b91
Hiroaki Kadomatsu <note103@gmail.com>
2023-06-19 01:56:04 +0900

    変数名の変更: filenames -> contents
    ディレクトリも対象に含むこと、コード内にfileという名称が多いことなどから

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -13,8 +13,8 @@ FILE_TYPE_MAP = {
 
 def main
   options = parse_options
-  filenames = current_directory_filenames
-  options[:l] ? print_long_format(filenames) : print_columns_format(filenames)
+  contents = current_directory_contents
+  options[:l] ? print_long_format(contents) : print_columns_format(contents)
 end
 
 def parse_options
@@ -28,14 +28,14 @@ def parse_options
   options
 end
 
-def current_directory_filenames
+def current_directory_contents
   Dir.glob('*')
 end
 
-def print_long_format(filenames)
+def print_long_format(contents)
   puts "total #{calculate_total_blocks}"
 
-  filenames.each do |file|
+  contents.each do |file|
     file_stat = File.stat(file)
     print FILE_TYPE_MAP.fetch(file_stat.ftype, '?')
     print_permissions(file_stat)
@@ -50,7 +50,7 @@ end
 
 def calculate_total_blocks
   total_blocks = 0
-  current_directory_filenames.each do |file|
+  current_directory_contents.each do |file|
     total_blocks += File.stat(file).blocks
   end
   total_blocks
@@ -69,17 +69,17 @@ def print_permissions_for_user_type(mode)
   print(mode & 1 == 1 ? 'x' : '-')
 end
 
-def print_columns_format(filenames)
-  max_length = filenames.map(&:length).max
-  formatted_filenames = filenames.map { |l| l.ljust(max_length + SPACE_BETWEEN_COLUMNS) }
-  row_count = (filenames.size.to_f / COLUMN).ceil
-  grid = create_grid_array(formatted_filenames, row_count)
+def print_columns_format(contents)
+  max_length = contents.map(&:length).max
+  formatted_contents = contents.map { |l| l.ljust(max_length + SPACE_BETWEEN_COLUMNS) }
+  row_count = (contents.size.to_f / COLUMN).ceil
+  grid = create_grid_array(formatted_contents, row_count)
   print_formatted_grid(grid)
 end
 
-def create_grid_array(filenames, row_count)
+def create_grid_array(contents, row_count)
   grid = Array.new(row_count) { Array.new(COLUMN) }
-  filenames.each_with_index do |l, i|
+  contents.each_with_index do |l, i|
     column, row = i.divmod(row_count)
     grid[row][column] = l
   end
