b0eb54be15d788c93e8f000624792620ce832cdb
Hiroaki Kadomatsu <note103@gmail.com>
2023-07-15 12:54:52 +0900

    全般的にリファクタリング

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -4,50 +4,59 @@ require 'optparse'
 
 def main
   options = parse_options
-  line_sum = word_sum = char_sum = 0
-
-  ARGV.each do |item|
-    line, word, char = process_file(item, options)
-    line_sum += line
-    word_sum += word
-    char_sum += char
-  end
-
-  return unless ARGV.size > 1
-
-  total = format_counts(line_sum, word_sum, char_sum, options)
-  puts "#{total} total"
-end
-
-def process_file(item, options)
-  text = File.read(item)
-  line = text.count("\n")
-  word = text.split.size
-  char = text.bytesize
-
-  result = format_counts(line, word, char, options)
-  puts "#{result} #{item}"
-
-  [line, word, char]
-end
-
-def format_counts(line, word, char, options)
-  result = []
-  result << line.to_s.rjust(8) if options.empty? || options[:l]
-  result << word.to_s.rjust(8) if options.empty? || options[:w]
-  result << char.to_s.rjust(8) if options.empty? || options[:c]
-  result.join('')
+  total_count = { line_sum: 0, word_sum: 0, char_sum: 0 }
+  ARGV.empty? ? handle_stdin(options) : handle_files(total_count, options)
 end
 
 def parse_options
   options = {}
   OptionParser.new do |opt|
-    opt.on('-l', 'Count lines') { |l| options[:l] = l }
-    opt.on('-w', 'Count words') { |w| options[:w] = w }
-    opt.on('-c', 'Count bytes') { |c| options[:c] = c }
+    opt.on('-l', 'Count lines') { options[:l] = true }
+    opt.on('-w', 'Count words') { options[:w] = true }
+    opt.on('-c', 'Count bytes') { options[:c] = true }
     opt.parse!(ARGV)
   end
   options
+end
+
+def handle_stdin(options)
+  text = $stdin.read
+  line, word, char = process_file(text)
+  puts format_counts(line, word, char, options)
+end
+
+def handle_files(total_count, options)
+  ARGV.each do |item|
+    text = File.read(item)
+    line, word, char = process_file(text)
+    puts format_counts(line, word, char, options, item)
+
+    total_count[:line_sum] += line
+    total_count[:word_sum] += word
+    total_count[:char_sum] += char
+  end
+  caluculate_total(total_count, options) if ARGV.size > 1
+end
+
+def process_file(text)
+  line = text.count("\n")
+  word = text.split.size
+  char = text.bytesize
+  [line, word, char]
+end
+
+def format_counts(line, word, char, options, filename = nil)
+  result = []
+  result << line.to_s.rjust(8) if options.empty? || options[:l]
+  result << word.to_s.rjust(8) if options.empty? || options[:w]
+  result << char.to_s.rjust(8) if options.empty? || options[:c]
+  result = result.join('')
+  filename ? "#{result} #{filename}" : result
+end
+
+def caluculate_total(total_count, options)
+  total = format_counts(total_count[:line_sum], total_count[:word_sum], total_count[:char_sum], options)
+  puts "#{total} total"
 end
 
 main
